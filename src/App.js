2b465e42eede0f77918c7f7308de4b3aeb0c0a2f
tcarey97 <125526091+tcarey97@users.noreply.github.com>
2023-11-30 20:09:16 +1100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Budget from './components/Budget';
 
 // Add code to import the other components here under
+import Remaining from './components/Remaining';
 
 
 import { AppProvider } from './context/AppContext';
@@ -16,11 +17,17 @@ const App = () => {
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
                         {
-                            /* Add Budget component here */
+                        // Budget component
+                            <div className='col-sm'>
+                                <Budget />
+                            </div>
                         }        
 
                         {
-                            /* Add Remaining component here*/
+                        //Remaining component
+                            <div className='col-sm'>
+                                <Remaining />
+                            </div>
                         }        
 
                         {
